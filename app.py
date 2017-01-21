39bdaa929c37fb3521150a85f37550af022044b5
Jing Yin Ong <ongjingyin@yahoo.com.sg>
2017-01-21 22:47:53 +0800

    Added drag and drop box, successful image uploading

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 from gevent.wsgi import WSGIServer
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def main():
 	return render_template('home.html')
 
-@app.route("/heatmap")
-def heatmap():
-	return render_template('predictionHeatmap.html')
-
-@app.route("/twitterheatmap")
-def twitterheatmap():
-	return render_template('twitterheatmap.html')
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return ''
+    return "upload rejected"
 
 @app.route("/visualize")
 def visualize():
