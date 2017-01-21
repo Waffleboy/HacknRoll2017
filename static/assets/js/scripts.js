0cc93288f9adc56375e4d6443b19e817a3da3c59
Jing Yin Ong <ongjingyin@yahoo.com.sg>
2017-01-22 05:23:49 +0800

    Changed background

--- static/assets/js/scripts.js
+++ static/assets/js/scripts.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
-    $('.top-content').backstretch("static/assets/img/backgrounds/1.jpg");
-    $('.single-feature-bg').backstretch("static/assets/img/backgrounds/1.jpg");
+    $('.top-content').backstretch("static/assets/img/backgrounds/2.jpg");
+    $('.single-feature-bg').backstretch("static/assets/img/backgrounds/2.jpg");
 
 });
