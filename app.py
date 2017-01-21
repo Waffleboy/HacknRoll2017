f9e6fa003df7511af497baec8619d80e25a8f73c
waffleboy <thiru1337@gmail.com>
2017-01-22 01:11:20 +0800

    update reqs

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 import cv2
+import glob
 from flask import Flask, render_template, request
 from gevent.wsgi import WSGIServer
 from werkzeug import secure_filename
 import pandas as pd
+import wgetter
 from keras.models import load_model
 from sklearn.externals import joblib
+from flask import jsonify
 
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -41,14 +44,28 @@ def upload_file():
     return "upload rejected"
 
 
+@app.route('/api/<path:path>')
+def messenger(path):
+    folder_name = "messengerpics"
+    check_or_make_folder(folder_name)
+    wgetter.download(path,outdir=folder_name)
+    file = glob.glob(folder_name+'/' + '*.jpg')[0]
+    pic = preprocess_single_image(file)
+    pred_class = model.predict_classes(pic)[0]
+    pred_class_name = get_pred_class_name(pred_class)
+    pred_class_extra_details_dic = get_pred_class_extra_details(pred_class_name)
+    pred_class_extra_details_dic["class"] = pred_class_name
+    return jsonify(pred_class_extra_details_dic)
+
 @app.route("/visualize")
 def visualize():
 	return "Coming soon!"
 
-def pred_class_extra_details(pred_class_name):
+
+def get_pred_class_extra_details(pred_class_name):
     df = load_and_format_extra_details_csv()
     df = df[df["Disease"] == pred_class_name]
-    return df.to_dict()
+    return df.to_dict(orient='records')[0]
     
 def get_pred_class_name(pred_class_number):
     global mapper
@@ -67,7 +84,10 @@ def preprocess_single_image(filepath):
     pic = pic.reshape(-1,3,120,120)
     return pic
     
-    
+def check_or_make_folder(foldername):
+    if not os.path.exists(foldername):
+        os.mkdir(foldername)
+
 if __name__ == "__main__":
     server = WSGIServer(("",5000), app)
     print('Server is up')
