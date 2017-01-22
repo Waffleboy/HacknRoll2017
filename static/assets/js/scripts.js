fb9d7ef495857d341f6a7d3dca322a9f8bd5c9e5
Jing Yin Ong <ongjingyin@yahoo.com.sg>
2017-01-22 11:13:18 +0800

    Background has plain left side; Changed font colors, margins, position

--- static/assets/js/scripts.js
+++ static/assets/js/scripts.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
     /*
         Fullscreen background
     */
-    $('.top-content').backstretch("static/assets/img/backgrounds/2.jpg");
-    $('.single-feature-bg').backstretch("static/assets/img/backgrounds/2.jpg");
+    $('.top-content').backstretch("static/assets/img/backgrounds/2.png");
+    // $('.single-feature-bg').backstretch("static/assets/img/backgrounds/2.png");
 
 });
